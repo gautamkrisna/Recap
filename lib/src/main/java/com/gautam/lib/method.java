03d206d26ab8215a816e3c9591055b47eaa4cb59
gautamkrisna <gautamkrisna@gmail.com>
2018-08-11 07:08:43 +0530

    test

--- lib/src/main/java/com/gautam/lib/method.java
+++ lib/src/main/java/com/gautam/lib/method.java
@@ -1,4 +1,26 @@
 package com.gautam.lib;
 
 public class method {
+    //main
+    public static void main ( String args [] ){
+        int x = 5, y = 6, result;
+        result = sum(x,y);
+        System.out.println("Results is - " +result);
+        // test declaration
+        dec();
+
+    }
+
+    //method with parameters
+    public static int sum (int a, int b ) {
+        int retVal;
+        retVal = a + b ;
+        return  retVal;
+    }
+
+    //method without any parameters
+    public static int dec (){
+        System.out.println("test Declaration");
+        return 0;
+    }
 }
